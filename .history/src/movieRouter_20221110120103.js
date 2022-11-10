57d467660d87369c32a10345e51746d16df5ec9c
Hess Wang <hess90wang@gmail.com>
2022-11-10 21:04:00 +0900

    a09

---
+++ .history/src/movieRouter_20221110120103.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { home, movieDetail, getAdd, postAdd } from "./movieController";
+
+const movieRouter = express.Router();
+
+// create the '/' route
+// create the /:id route
+// create the /add route (GET + POST)
+movieRouter.get("/", home);
+movieRouter.route("/add").get(getAdd).post(postAdd);
+movieRouter.get("/:id", movieDetail);
+export default movieRouter;
