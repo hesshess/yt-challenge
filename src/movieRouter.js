de9d976a5da8a73405eeec38e91800c07e74ba55
Hess Wang <hess90wang@gmail.com>
2022-11-11 13:07:09 +0900

    a09solution

--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import { home, movieDetail, getAdd, postAdd } from './movieController';
+import express from "express";
+import { home, movieDetail, getAdd, postAdd } from "./movieController";
 
 const movieRouter = express.Router();
 
-movieRouter.get('/', home);
-movieRouter.route('/add').get(getAdd).post(postAdd);
-movieRouter.get('/:id', movieDetail);
+// create the '/' route
+// create the /:id route
+// create the /add route (GET + POST)
+movieRouter.get("/", home);
+movieRouter.route("/add").get(getAdd).post(postAdd);
+movieRouter.get("/:id", movieDetail);
 export default movieRouter;
